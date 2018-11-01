0f309e10d11e15de7b092f11388dbb000c21e58f
sujunhao <spike76@qq.com>
2018-11-01 09:59:47 +0800

    文件重组织：合并同一问题的不同解决到同一文件

--- 6_zigzag_conversion.rb
+++ 6_zigzag_conversion.rb
@@ -1,3 +1,25 @@
+#faster solution
+def convert(s, num_rows)
+  return s if num_rows >= s.size || num_rows == 1
+  s = s.chars
+  rows = ''
+  num_rows.times do |n|
+    row = ''
+    pos = n
+    while s[pos]
+      row << s[pos]
+      if n != 0 && n != num_rows - 1
+        zig_pos = pos + (num_rows - n - 1) * 2
+        row << s[zig_pos] if s[zig_pos]
+      end
+      pos += 2 * num_rows - 2
+    end
+    rows << row
+  end
+  rows
+end
+
+#slower sulotion
 def convert(s, num_rows)
   return s if num_rows >= s.size
   cols = []
